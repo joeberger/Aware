b6bb9b3771415b05f30127d09e567679da71ab59
joeberger <jberger@chariotsolutions.com>
2013-11-01 14:28:20 -0400

    initial commit
    customized from MEAN stack repo

---
+++ public/js/app.js
@@ -0,0 +1,4 @@
+window.app = angular.module('aware', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'aware.system', 'aware.articles']);
+
+angular.module('aware.system', []);
+angular.module('aware.articles', []);
